d304aa32f57cc931a7a93fd8a224db35f52cc996
2bumb <alee62580@gmail.com>
2024-02-26 19:03:18 -0800

    added changes

--- script.js
+++ script.js
@@ -8,28 +8,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const tylerVid = document.querySelector(".tyler-vid");
 
     function resetNoBtnStyle() {
-        noBtn.style.transform = ""; // Reset the transform
+        noBtn.style.transform = "";  
     }
 
     yesBtn.addEventListener("click", function() {
         img1.src = img2.src;
-        // Change background for 7 seconds
         document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/cf/50/6d/cf506d6998d68de01e9171f30fc4e287.gif)";
         setTimeout(function() {
-            document.body.style.backgroundImage = "none"; // Reset background after 7 seconds
+            document.body.style.backgroundImage = "none";  
             img2.classList.remove('hidden');
             yayVideo.play();
-            img1.src = "kakashi-death.gif"; // Reset img-2 after 7 seconds
-        }, 2000); // 7 seconds
+            img1.src = "kakashi-death.gif";  
+        }, 2000);  
         setTimeout(function() {
             
-            img1.src = "obito.gif"; // Reset img-1 source to empty after additional 7 seconds
-            resetNoBtnStyle(); // Reset the style of the no button
-        }, 4800); // 7 seconds
+            img1.src = "obito.gif";  
+            resetNoBtnStyle();  
+        }, 4800);  
     });
 
     noBtn.addEventListener("mouseover", function() {
-        noBtn.style.transform = "translateX(-470px)"; // Move the button 300px to the right
+        noBtn.style.transform = "translateX(-470px)";  
     });
 
     noBtn.addEventListener("mouseout", function() {
